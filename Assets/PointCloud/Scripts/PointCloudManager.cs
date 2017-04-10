e439e9a34ba4d6abf9b8ba38afa55728380bdc8e
Wenbo Lan <info@wenbolan.com>
2017-04-10 13:51:44 -0400

    init

---
+++ Assets/PointCloud/Scripts/PointCloudManager.cs
@@ -0,0 +1,266 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class PointCloudManager : MonoBehaviour {
+
+    //Play
+    public bool play;
+
+	// File
+	public string dataPath;
+	private string filename;
+	public Material matVertex;
+    public int IndexNumber;
+
+	// GUI
+	private float progress = 0;
+	private string guiText;
+	private bool loaded = false;
+
+    //Coroutine
+    [SerializeField]
+    private bool loadOffEnd;
+    [SerializeField]
+    private int index;
+
+	// PointCloud
+	private GameObject pointCloud;
+
+
+	public float scale = 1;
+	public bool invertYZ = false;
+	public bool forceReload = false;
+
+	public int numPoints;
+	public int numPointGroups;
+	private int limitPoints = 65000;
+
+	private Vector3[] points;
+	private Color[] colors;
+	private Vector3 minValue;
+
+	
+	void Start () {
+		// Create Resources folder
+		createFolders ();
+        loadOffEnd = true;
+        //index = 0;
+		// Get Filename
+		filename = Path.GetFileName(dataPath) + index;
+		//loadScene ();
+        if (play)
+        {
+            loaded = true;
+            StartCoroutine(Play());
+        }
+
+
+    }
+
+    void Update()
+    {
+
+        if (loadOffEnd && index < IndexNumber && !play)
+        {
+            index++;
+            filename = Path.GetFileName(dataPath) + index;
+            loadScene();
+        }
+    }
+
+    void loadScene(){
+        // Check if the PointCloud was loaded previously
+#if UNITY_EDITOR
+        if (!Directory.Exists (Application.dataPath + "/Resources/PointCloudMeshes/" + filename)){
+			UnityEditor.AssetDatabase.CreateFolder ("Assets/Resources/PointCloudMeshes", filename);
+			loadPointCloud ();
+		} else if (forceReload){
+			UnityEditor.FileUtil.DeleteFileOrDirectory(Application.dataPath + "/Resources/PointCloudMeshes/" + filename);
+			UnityEditor.AssetDatabase.Refresh();
+			UnityEditor.AssetDatabase.CreateFolder ("Assets/Resources/PointCloudMeshes", filename);
+			loadPointCloud ();
+		} else
+			// Load stored PointCloud
+			loadStoredMeshes();
+#endif
+    }
+	
+	
+	void loadPointCloud(){
+		// Check what file exists
+		if (File.Exists (Application.dataPath + dataPath +index+ ".off")) 
+			// load off
+			StartCoroutine ("loadOFF", dataPath +index + ".off");
+		else 
+			Debug.Log ("File '" + dataPath + index+ "' could not be found"); 
+		
+	}
+	
+	// Load stored PointCloud
+	void loadStoredMeshes(){
+
+		Debug.Log ("Using previously loaded PointCloud: " + filename);
+
+		GameObject pointGroup = Instantiate(Resources.Load ("PointCloudMeshes/" + filename)) as GameObject;
+
+		loaded = true;
+	}
+
+    IEnumerator Play()
+    {
+        for (int i = index; i <= IndexNumber; i++)
+        {
+            filename =  Path.GetFileName(dataPath) + i;
+            print(filename);
+            Debug.Log("Using previously loaded PointCloud: " + filename);
+
+            GameObject pointGroup = Instantiate(Resources.Load("PointCloudMeshes/" + filename)) as GameObject;
+
+            yield return new WaitForSeconds(0.1f);
+            Destroy(pointGroup);
+
+        }
+    }
+	
+	// Start Coroutine of reading the points from the OFF file and creating the meshes
+	IEnumerator loadOFF(string dPath){
+        loadOffEnd = false;
+		// Read file
+		StreamReader sr = new StreamReader (Application.dataPath + dPath);
+		sr.ReadLine (); // OFF
+		string[] buffer = sr.ReadLine ().Split(); // nPoints, nFaces
+		
+		numPoints = int.Parse (buffer[0]);
+		points = new Vector3[numPoints];
+		colors = new Color[numPoints];
+		minValue = new Vector3();
+		
+		for (int i = 0; i< numPoints; i++){
+			buffer = sr.ReadLine ().Split ();
+
+			if (!invertYZ)
+				points[i] = new Vector3 (float.Parse (buffer[0])*scale, float.Parse (buffer[1])*scale,float.Parse (buffer[2])*scale) ;
+			else
+				points[i] = new Vector3 (float.Parse (buffer[0])*scale, float.Parse (buffer[2])*scale,float.Parse (buffer[1])*scale) ;
+			
+			if (buffer.Length >= 5)
+				colors[i] = new Color (int.Parse (buffer[3])/255.0f,int.Parse (buffer[4])/255.0f,int.Parse (buffer[5])/255.0f);
+			else
+				colors[i] = Color.cyan;
+
+			// Relocate Points near the origin
+			//calculateMin(points[i]);
+
+			// GUI
+			progress = i *1.0f/(numPoints-1)*1.0f;
+			if (i%Mathf.FloorToInt(numPoints/20) == 0){
+				guiText=i.ToString() + " out of " + numPoints.ToString() + " loaded";
+				yield return null;
+			}
+            
+		}
+
+		
+		// Instantiate Point Groups
+		numPointGroups = Mathf.CeilToInt (numPoints*1.0f / limitPoints*1.0f);
+
+		pointCloud = new GameObject (filename);
+
+		for (int i = 0; i < numPointGroups-1; i ++) {
+			InstantiateMesh (i, limitPoints);
+			if (i%10==0){
+				guiText = i.ToString() + " out of " + numPointGroups.ToString() + " PointGroups loaded";
+				yield return null;
+			}
+		}
+		InstantiateMesh (numPointGroups-1, numPoints- (numPointGroups-1) * limitPoints);
+
+#if UNITY_EDITOR
+        //Store PointCloud
+        UnityEditor.PrefabUtility.CreatePrefab ("Assets/Resources/PointCloudMeshes/" + filename + ".prefab", pointCloud);
+#endif
+        loaded = true;
+        loadOffEnd = true;
+    }
+
+	
+	void InstantiateMesh(int meshInd, int nPoints){
+		// Create Mesh
+		GameObject pointGroup = new GameObject (filename + meshInd);
+		pointGroup.AddComponent<MeshFilter> ();
+		pointGroup.AddComponent<MeshRenderer> ();
+		pointGroup.GetComponent<Renderer>().material = matVertex;
+
+		pointGroup.GetComponent<MeshFilter> ().mesh = CreateMesh (meshInd, nPoints, limitPoints);
+		pointGroup.transform.parent = pointCloud.transform;
+
+#if UNITY_EDITOR
+        // Store Mesh
+        UnityEditor.AssetDatabase.CreateAsset(pointGroup.GetComponent<MeshFilter> ().mesh, "Assets/Resources/PointCloudMeshes/" + filename + @"/" + filename + meshInd + ".asset");
+		UnityEditor.AssetDatabase.SaveAssets ();
+		UnityEditor.AssetDatabase.Refresh();
+#endif
+    }
+
+	Mesh CreateMesh(int id, int nPoints, int limitPoints){
+		
+		Mesh mesh = new Mesh ();
+		
+		Vector3[] myPoints = new Vector3[nPoints]; 
+		int[] indecies = new int[nPoints];
+		Color[] myColors = new Color[nPoints];
+
+		for(int i=0;i<nPoints;++i) {
+			myPoints[i] = points[id*limitPoints + i] - minValue;
+			indecies[i] = i;
+			myColors[i] = colors[id*limitPoints + i];
+		}
+
+
+		mesh.vertices = myPoints;
+		mesh.colors = myColors;
+		mesh.SetIndices(indecies, MeshTopology.Points,0);
+		mesh.uv = new Vector2[nPoints];
+		mesh.normals = new Vector3[nPoints];
+
+
+		return mesh;
+	}
+
+	void calculateMin(Vector3 point){
+		if (minValue.magnitude == 0)
+			minValue = point;
+
+
+		if (point.x < minValue.x)
+			minValue.x = point.x;
+		if (point.y < minValue.y)
+			minValue.y = point.y;
+		if (point.z < minValue.z)
+			minValue.z = point.z;
+	}
+
+	void createFolders(){
+#if UNITY_EDITOR
+        if (!Directory.Exists (Application.dataPath + "/Resources/"))
+			UnityEditor.AssetDatabase.CreateFolder ("Assets", "Resources");
+
+		if (!Directory.Exists (Application.dataPath + "/Resources/PointCloudMeshes/"))
+			UnityEditor.AssetDatabase.CreateFolder ("Assets/Resources", "PointCloudMeshes");
+#endif
+    }
+
+
+	void OnGUI(){
+
+
+		if (!loaded){
+			GUI.BeginGroup (new Rect(Screen.width/2-100, Screen.height/2, 400.0f, 20));
+			GUI.Box (new Rect (0, 0, 200.0f, 20.0f), guiText);
+			GUI.Box (new Rect (0, 0, progress*200.0f, 20), "");
+			GUI.EndGroup ();
+		}
+	}
+
+}
---
+++ Kinect Recorder/Kinect/KinectRecorder.cs
@@ -0,0 +1,396 @@
+﻿/**
+The MIT License (MIT)
+
+Copyright (c) 2014 Lars Ivar Hatledal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+**/
+
+
+// Modified Kinect sample code, which enabled the recording of colored 3D point clouds
+// Recordings are saved as zipped .ply files, where the whole .zip file correspond to a 'movie clip'.
+// Single .ply files may be extracted and visualized in a software like MeshLab
+//
+// http://laht.info
+
+namespace laht.info
+{
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading;
+    using System.Collections;
+
+
+    public class KinectRecorder
+    {
+
+        private string path = @"cloud" + DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local).ToShortDateString() + ".zip";
+
+        private const int minNumOfPoints = 10000;
+        private const int maxNumOfFrames = 2000;
+        private const int step = 2;
+
+        private bool done = false;
+        private int count = 0;
+
+        private ArrayList clouds = new ArrayList();
+        private Stopwatch t0 = new Stopwatch();
+
+
+        /// <summary>
+        /// Indicates opaque in an opacity mask
+        /// </summary>
+        private const int OpaquePixel = -1;
+
+        /// <summary>
+        /// Size of the RGB pixel in the bitmap
+        /// </summary>
+        private readonly int bytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor kinectSensor = null;
+
+        /// <summary>
+        /// Coordinate mapper to map one type of point to another
+        /// </summary>
+        private CoordinateMapper coordinateMapper = null;
+
+        /// <summary>
+        /// Reader for depth/color/body index frames
+        /// </summary>
+        private MultiSourceFrameReader multiFrameSourceReader = null;
+
+        /// <summary>
+        /// Intermediate storage for receiving depth frame data from the sensor
+        /// </summary>
+        private ushort[] depthFrameData = null;
+
+        /// <summary>
+        /// Intermediate storage for receiving color frame data from the sensor
+        /// </summary>
+        private byte[] colorFrameData = null;
+
+        /// <summary>
+        /// Intermediate storage for receiving body index frame data from the sensor
+        /// </summary>
+        private byte[] bodyIndexFrameData = null;
+
+        /// <summary>
+        /// Intermediate storage for the depth to color mapping
+        /// </summary>
+        private ColorSpacePoint[] colorPoints = null;
+
+        /// <summary>
+        /// Intermediate storage for the depth to color mapping
+        /// </summary>
+        private CameraSpacePoint[] cameraPoints = null;
+
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public KinectRecorder()
+        {
+
+            //start clock
+            this.t0.Start();
+
+            // get the kinectSensor object
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // open multiframereader for depth, color, and bodyindex frames
+            this.multiFrameSourceReader = this.kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Depth | FrameSourceTypes.Color | FrameSourceTypes.BodyIndex);
+
+            // wire handler for frames arrival
+            this.multiFrameSourceReader.MultiSourceFrameArrived += this.Reader_MultiSourceFrameArrived;
+
+            // get the coordinate mapper
+            this.coordinateMapper = this.kinectSensor.CoordinateMapper;
+
+            // get FrameDescription from DepthFrameSource
+            FrameDescription depthFrameDescription = this.kinectSensor.DepthFrameSource.FrameDescription;
+
+            int depthWidth = depthFrameDescription.Width;
+            int depthHeight = depthFrameDescription.Height;
+
+            // allocate space to put the pixels being received and converted
+            this.depthFrameData = new ushort[depthWidth * depthHeight];
+            this.bodyIndexFrameData = new byte[depthWidth * depthHeight];
+            this.colorPoints = new ColorSpacePoint[depthWidth * depthHeight];
+            this.cameraPoints = new CameraSpacePoint[depthWidth * depthHeight];
+
+            // get FrameDescription from ColorFrameSource
+            FrameDescription colorFrameDescription = this.kinectSensor.ColorFrameSource.FrameDescription;
+
+            int colorWidth = colorFrameDescription.Width;
+            int colorHeight = colorFrameDescription.Height;
+
+            // allocate space to put the pixels being received
+            this.colorFrameData = new byte[colorWidth * colorHeight * this.bytesPerPixel];
+
+            // open the sensor
+            this.kinectSensor.Open();
+
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadLine();
+
+            Console.WriteLine("Flushing data to disk..");
+            Console.WriteLine(this.path);
+            using (FileStream zipToOpen = new FileStream(this.path, FileMode.Create))
+            {
+                using (ZipArchive zipArchive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    for (int i = 0; i < clouds.Count; i++)
+                    {
+                        ZipArchiveEntry zipEntry = zipArchive.CreateEntry("cloud" + i + ".off");
+                        using (StreamWriter writer = new StreamWriter(zipEntry.Open()))
+                        {
+                            writer.WriteLine(clouds[i]);
+                        }
+                        Console.WriteLine("Flushing " + i + " of " + clouds.Count);
+                    }
+                    Console.WriteLine("Shuting down..");
+                }
+            }
+
+
+            this.multiFrameSourceReader.Dispose();
+            this.multiFrameSourceReader = null;
+
+            if (this.kinectSensor != null)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Handles the depth/color/body index frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            if (!done)
+            {
+                if (t0.ElapsedMilliseconds > 41)
+                {
+                     //Console.WriteLine(t0.ElapsedMilliseconds);
+                    t0.Restart();
+
+                    int depthWidth = 0;
+                    int depthHeight = 0;
+
+                    int colorWidth = 0;
+                    int colorHeight = 0;
+
+                    int bodyIndexWidth = 0;
+                    int bodyIndexHeight = 0;
+
+                    bool multiSourceFrameProcessed = false;
+                    bool colorFrameProcessed = false;
+                    bool depthFrameProcessed = false;
+                    bool bodyIndexFrameProcessed = false;
+
+                    MultiSourceFrame multiSourceFrame = e.FrameReference.AcquireFrame();
+
+                    if (multiSourceFrame != null)
+                    {
+                        // Frame Acquisition should always occur first when using multiSourceFrameReader
+                        using (DepthFrame depthFrame = multiSourceFrame.DepthFrameReference.AcquireFrame())
+                        {
+                            using (ColorFrame colorFrame = multiSourceFrame.ColorFrameReference.AcquireFrame())
+                            {
+                                using (BodyIndexFrame bodyIndexFrame = multiSourceFrame.BodyIndexFrameReference.AcquireFrame())
+                                {
+                                    if (depthFrame != null)
+                                    {
+                                        FrameDescription depthFrameDescription = depthFrame.FrameDescription;
+                                        depthWidth = depthFrameDescription.Width;
+                                        depthHeight = depthFrameDescription.Height;
+
+                                        if ((depthWidth * depthHeight) == this.depthFrameData.Length)
+                                        {
+                                            depthFrame.CopyFrameDataToArray(this.depthFrameData);
+
+                                            depthFrameProcessed = true;
+                                        }
+                                    }
+
+                                    if (colorFrame != null)
+                                    {
+                                        FrameDescription colorFrameDescription = colorFrame.FrameDescription;
+                                        colorWidth = colorFrameDescription.Width;
+                                        colorHeight = colorFrameDescription.Height;
+
+                                        if ((colorWidth * colorHeight * this.bytesPerPixel) == this.colorFrameData.Length)
+                                        {
+                                            if (colorFrame.RawColorImageFormat == ColorImageFormat.Bgra)
+                                            {
+                                                colorFrame.CopyRawFrameDataToArray(this.colorFrameData);
+                                            }
+                                            else
+                                            {
+                                                colorFrame.CopyConvertedFrameDataToArray(this.colorFrameData, ColorImageFormat.Bgra);
+                                            }
+
+                                            colorFrameProcessed = true;
+                                        }
+                                    }
+
+                                    if (bodyIndexFrame != null)
+                                    {
+                                        FrameDescription bodyIndexFrameDescription = bodyIndexFrame.FrameDescription;
+                                        bodyIndexWidth = bodyIndexFrameDescription.Width;
+                                        bodyIndexHeight = bodyIndexFrameDescription.Height;
+
+                                        if ((bodyIndexWidth * bodyIndexHeight) == this.bodyIndexFrameData.Length)
+                                        {
+                                            bodyIndexFrame.CopyFrameDataToArray(this.bodyIndexFrameData);
+
+                                            bodyIndexFrameProcessed = true;
+                                        }
+                                    }
+                                    multiSourceFrameProcessed = true;
+                                }
+                            }
+                        }
+                    }
+
+
+
+                    // we got all frames
+                    if (multiSourceFrameProcessed && depthFrameProcessed && colorFrameProcessed && bodyIndexFrameProcessed)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        int len = 0;
+
+                        this.coordinateMapper.MapDepthFrameToColorSpace(this.depthFrameData, this.colorPoints);
+                        this.coordinateMapper.MapDepthFrameToCameraSpace(this.depthFrameData, this.cameraPoints);
+
+                        // loop over each row and column of the depth
+                        for (int y = 0; y < depthHeight; y += step)
+                        {
+                            for (int x = 0; x < depthWidth; x += step)
+                            {
+
+                                // calculate index into depth array
+                                int depthIndex = (y * depthWidth) + x;
+
+                                byte player = this.bodyIndexFrameData[depthIndex];
+
+                                // if we're tracking a player for the current pixel, sets its color and alpha to full
+                                if (player != 0xff)
+                                {
+
+                                    CameraSpacePoint p = this.cameraPoints[depthIndex];
+
+                                    // retrieve the depth to color mapping for the current depth pixel
+                                    ColorSpacePoint colorPoint = this.colorPoints[depthIndex];
+
+                                    byte r = 0; byte g = 0; byte b = 0;
+
+                                    // make sure the depth pixel maps to a valid point in color space
+                                    int colorX = (int)Math.Floor(colorPoint.X + 0.5);
+                                    int colorY = (int)Math.Floor(colorPoint.Y + 0.5);
+                                    if ((colorX >= 0) && (colorX < colorWidth) && (colorY >= 0) && (colorY < colorHeight))
+                                    {
+                                        // calculate index into color array
+                                        int colorIndex = ((colorY * colorWidth) + colorX) * this.bytesPerPixel;
+
+                                        // set source for copy to the color pixel
+                                        int displayIndex = depthIndex * this.bytesPerPixel;
+
+                                        b = this.colorFrameData[colorIndex++];
+                                        g = this.colorFrameData[colorIndex++];
+                                        r = this.colorFrameData[colorIndex++];
+
+                                    }
+
+                                    if (!(Double.IsInfinity(p.X)) && !(Double.IsInfinity(p.Y)) && !(Double.IsInfinity(p.Z)))
+                                    {
+                                        sb.Append(String.Format(CultureInfo.InvariantCulture, "{0} {1} {2} {3} {4} {5}\n", p.X, p.Y, p.Z, r, g, b,255));
+                                        len++;
+                                    }
+                                }
+                            }
+                        }
+
+                        if (len > minNumOfPoints && count < maxNumOfFrames)
+                        {
+                            //String header = "ply \n" +
+                            //            "format ascii 1.0 \n" +
+                            //            "element vertex " + len + "\n" +
+                            //            "property float x \n" +
+                            //            "property float y \n" +
+                            //            "property float z \n" +
+                            //            "property uchar red \n" +
+                            //            "property uchar green \n" +
+                            //            "property uchar blue \n" +
+                            //            "end_header \n";
+                            String header = "COFF\n" +
+                                            len + " " + len * 4 + "\n";
+                            sb.Insert(0, header);
+
+                            WriteFile(count, sb);
+                            //clouds.Add(sb.ToString());
+                            Console.WriteLine("Wrote cloud " + count);
+                            count++;
+                        }
+                        else if (count == maxNumOfFrames)
+                        {
+                            Console.WriteLine("Done.. Press any key to exit");
+                            done = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            Thread.Sleep(2000);
+            new KinectRecorder();
+        }
+
+        public static void WriteFile(int c, StringBuilder s)
+        {
+            string pp = @"cloud\" + c + ".off";
+            FileStream FileToOpen = File.Create(pp);
+            StreamWriter writer = new StreamWriter(FileToOpen);
+            writer.WriteLine(s.ToString());
+            writer.Close();
+;            FileToOpen.Close();
+        }
+    }
+}
